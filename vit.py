a941036ddf52e4cb07bc436528ce25d4f3ab078b
Inigo Val Slijepcevic <inigoval@gmail.com>
2022-12-12 10:25:03 +0000

    Add ViT

--- vit.py
+++ vit.py
@@ -67,7 +67,8 @@ class Attention(nn.Module):
         )
 
     def forward(self, x):
-        # Split into q, k and v chunks
+        # Split into q, k and v chunks - qkv is a tuple (q, k ,v)
+        # Each q, k, v has dimension (batch_size, n_patches, dim)
         qkv = self.to_qkv(x).chunk(3, dim=-1)
 
         # Split q, k, v across multiple heads
@@ -92,13 +93,7 @@ class Attention(nn.Module):
 
 class Transformer(nn.Module):
     def __init__(
-        self,
-        dim,
-        depth,
-        heads,
-        dim_head,
-        mlp_dim,
-        dropout=0.0,
+        self, dim, depth, heads, dim_head, mlp_dim, dropout=0.0, attention_layer=Attention
     ):
         super().__init__()
 
@@ -107,7 +102,10 @@ class Transformer(nn.Module):
             self.layers.append(
                 nn.ModuleList(
                     [
-                        PreNorm(dim, Attention(dim, heads=heads, dim_head=dim_head, dropout=dropout)),
+                        PreNorm(
+                            dim,
+                            attention_layer(dim, heads=heads, dim_head=dim_head, dropout=dropout),
+                        ),
                         PreNorm(dim, FeedForward(dim, mlp_dim, dropout=dropout)),
                     ]
                 )
@@ -162,7 +160,9 @@ class ViT(nn.Module):
         # Turn (c x h x w) images into (n_patchs x patch_dim) flattened patches and
         # project to latent dimension
         self.to_patch_embedding = nn.Sequential(
-            Rearrange("b c (n1 p1) (n2 p2) -> b (n1 n2) (p1 p2 c)", p1=patch_height, p2=patch_width),
+            Rearrange(
+                "b c (n1 p1) (n2 p2) -> b (n1 n2) (p1 p2 c)", p1=patch_height, p2=patch_width
+            ),
             nn.Linear(patch_dim, dim),
         )
 
@@ -214,155 +214,3 @@ class ViT(nn.Module):
         # return self.mlp_head(x)
 
         return x
-
-
-#############################
-## ViT for small data-sets ##
-#############################
-
-
-# class ViT(nn.Module):
-#     def __init__(
-#         self,
-#         *,
-#         image_size,
-#         patch_size,
-#         # num_classes,
-#         dim,
-#         depth,
-#         heads,
-#         mlp_dim,
-#         channels=3,
-#         dim_head=64,
-#         dropout=0.0,
-#         emb_dropout=0.0,
-#         arch="vit"  # ['vit', 'vit-small']
-#     ):
-#         super().__init__()
-#         # If image isn't square, get dimensions from tuple
-#         # otherwise for square image just copy single dimension for square image
-#         image_height, image_width = pair(image_size)
-#         patch_height, patch_width = pair(patch_size)
-
-#         # Check that image can be split up into patches of given size
-#         assert (
-#             image_height % patch_height == 0 and image_width % patch_width == 0
-#         ), "Image dimensions must be divisible by the patch size."
-
-#         # Calculate total number of patches
-#         num_patches = (image_height // patch_height) * (image_width // patch_width)
-
-#         # Calculate total number of values in each patch
-#         patch_dim = channels * patch_height * patch_width
-
-#         # Turn (c x h x w) images into (n_patchs x patch_dim) flattened patches
-#         # self.to_patch = nn.Sequential(
-#         # Rearrange("b c (n1 p1) (n2 p2) -> b (n1 n2) (p1 p2 c)", p1=patch_height, p2=patch_width),
-#         # )
-
-#         # project to latent dimension
-#         self.to_patch = Rearrange(
-#             "b c (n1 p1) (n2 p2) -> b (n1 n2) (p1 p2 c)", p1=patch_height, p2=patch_width
-#         )
-
-#         if arch == "vit":
-#             self.to_patch_emb = nn.Sequential(
-#                 Rearrange("b c (n1 p1) (n2 p2) -> b (n1 n2) (p1 p2 c)", p1=patch_height, p2=patch_width),
-#                 nn.Linear(patch_dim, dim),
-#             )
-#         elif arch == "vit-small":
-#             self.to_patch_emb = SPT(dim=dim, patch_size=patch_size, channels=channels)
-
-#         # Positional embedding is learned in ViT.
-#         # Note that timm implementation multiplies normal distribution initialization by 0.02
-#         self.pos_embedding = nn.Parameter(torch.randn(1, num_patches + 1, dim))
-
-#         # Initialize cls token which is used as representation
-#         self.cls_token = nn.Parameter(torch.randn(1, 1, dim))
-#         self.dropout = nn.Dropout(emb_dropout)
-
-#         # Initialize transformer with given parameters
-#         self.transformer = Transformer(dim, depth, heads, dim_head, mlp_dim, dropout)
-
-#         self.to_latent = nn.Identity()
-
-#         # self.mlp_head = nn.Sequential(nn.LayerNorm(dim), nn.Linear(dim, num_classes))
-
-#     def forward(self, x):
-#         # Turn image into flattened patches
-#         x = self.to_patch_emb(x)
-
-#         # Get batch size and number of patches
-#         b, n, _ = x.shape
-
-#         # Expand cls token to correct size
-#         cls_tokens = repeat(self.cls_token, "1 1 d -> b 1 d", b=b)
-
-#         # Combine cls tokens with patches
-#         x = torch.cat((cls_tokens, x), dim=1)
-
-#         # Add positional embedding and dropout
-#         x += self.pos_embedding[:, : (n + 1)]
-#         x = self.dropout(x)
-
-#         # Run embedding through transformer
-#         x = self.transformer(x)
-
-#         # Get new class token
-#         x = x[:, 0]
-
-#         #  Return class token after passing through MLP
-#         x = self.to_latent(x)
-#         # return self.mlp_head(x)
-
-#         return x
-
-
-class SPT(nn.Module):
-    def __init__(self, *, dim, patch_size, channels=3):
-        super().__init__()
-        patch_dim = patch_size * patch_size * 5 * channels
-
-        self.to_patch_tokens = nn.Sequential(
-            Rearrange("b c (h p1) (w p2) -> b (h w) (p1 p2 c)", p1=patch_size, p2=patch_size),
-            nn.LayerNorm(patch_dim),
-            nn.Linear(patch_dim, dim),
-        )
-
-    def forward(self, x):
-        shifts = ((1, -1, 0, 0), (-1, 1, 0, 0), (0, 0, 1, -1), (0, 0, -1, 1))
-        shifted_x = list(map(lambda shift: F.pad(x, shift), shifts))
-        x_with_shifts = torch.cat((x, *shifted_x), dim=1)
-        return self.to_patch_tokens(x_with_shifts)
-
-
-class LSA(nn.Module):
-    def __init__(self, dim, heads=8, dim_head=64, dropout=0.0):
-        super().__init__()
-        inner_dim = dim_head * heads
-        self.heads = heads
-        self.temperature = nn.Parameter(torch.log(torch.tensor(dim_head**-0.5)))
-
-        self.attend = nn.Softmax(dim=-1)
-        self.dropout = nn.Dropout(dropout)
-
-        self.to_qkv = nn.Linear(dim, inner_dim * 3, bias=False)
-
-        self.to_out = nn.Sequential(nn.Linear(inner_dim, dim), nn.Dropout(dropout))
-
-    def forward(self, x):
-        qkv = self.to_qkv(x).chunk(3, dim=-1)
-        q, k, v = map(lambda t: rearrange(t, "b n (h d) -> b h n d", h=self.heads), qkv)
-
-        dots = torch.matmul(q, k.transpose(-1, -2)) * self.temperature.exp()
-
-        mask = torch.eye(dots.shape[-1], device=dots.device, dtype=torch.bool)
-        mask_value = -torch.finfo(dots.dtype).max
-        dots = dots.masked_fill(mask, mask_value)
-
-        attn = self.attend(dots)
-        attn = self.dropout(attn)
-
-        out = torch.matmul(attn, v)
-        out = rearrange(out, "b h n d -> b n (h d)")
-        return self.to_out(out)
